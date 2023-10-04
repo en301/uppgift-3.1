8ab1ea22e281c27e234d14787b268427a7874b74
emsj06001 <emsj06001@E-PW024NWP>
2023-10-04 10:31:02 +0200

    Add project files.

---
+++ uppgift 3.1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace uppgift_3_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur gammal är du?");
+            string ålder = Console.ReadLine();
+
+            int siffra = int.Parse(ålder);
+
+            if (siffra >= 16 & siffra <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+            Console.ReadKey();
+        }
+    }
+}
